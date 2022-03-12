c46bf378797764aba62a301dc04bd9ce733a8c7d
pyriteC <89157444+pyriteC@users.noreply.github.com>
2022-03-12 09:32:11 -0800

    moved the set method calls to initialize instead of execute

--- src/main/java/frc/robot/commands/Intake.java
+++ src/main/java/frc/robot/commands/Intake.java
@@ -5,20 +5,45 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.IntakeWheels;
+import frc.robot.subsystems.Pneumatics;
 
-public class PneumaticsUp extends CommandBase {
+public class Intake extends CommandBase {
   /** Creates a new PneumaticsUp. */
-  public PneumaticsUp() {
+  private boolean isUp = true;
+  private Pneumatics m_pneumatics;
+  private IntakeWheels m_intakeWheels;
+  public Intake(Pneumatics p, IntakeWheels i ) {
     // Use addRequirements() here to declare subsystem dependencies.
+    m_pneumatics = p;
+    m_intakeWheels = i;
+    addRequirements(m_pneumatics, m_intakeWheels);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() 
+  {
+    isUp = !isUp;
+    if (isUp = true)
+    {
+      m_pneumatics.solenoidDown();
+      m_intakeWheels.stopIntakeMotor();
+    }
+    else 
+    {
+      m_pneumatics.solenoidUp();
+      m_intakeWheels.setIntakeMotor();
+    }
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() 
+  {
+    
+
+  }
 
   // Called once the command ends or is interrupted.
   @Override
